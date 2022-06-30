24ddadee48ff06527475e7d6d04d67a02e87c352
RBeNturi <ridgemuturi@gmail.com>
2022-06-30 18:44:23 +0300

    Class Book

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Defining{
+    class Program{
+        static void Main(string[] args){
+            Book b1=new Book("War and Peace","Leo",825);
+            Book b2=new Book("Love and Hope","Dan",563);
+            Book b3=new Book("Inferno","Brown",678);
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
+            Console.WriteLine(b3.GetDescription());
+        }
+    }
+}
